327a10ad49c7fe1f0f1941c091e5af270d093202
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-08-23 16:22:24 -0400

    ice cream flavours array

---
+++ iceCreamFlavours.js
@@ -0,0 +1,21 @@
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+
+//add a new flavour
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//get the first flavour
+console.log(iceCreamFlavours[0]);
+
+//get the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//get the total number of flavours
+console.log(iceCreamFlavours.length);
+
