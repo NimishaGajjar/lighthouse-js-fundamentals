58e214d991f1ff3a5c6e7a9297f9c3a229c9d038
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-09-14 22:39:47 -0400

    practice done

--- whichSchool.js
+++ whichSchool.js
@@ -1,9 +1,12 @@
 function whichSchool(age) {
+
   if (age < 13) {
     console.log("Elementary School");
-  } else if (age >= 13 && age <= 18) {
-    console.log("Secondary School");
-  } else {
+  }
+  else if (age >= 13 && age <= 18) {
+    console.log('Secondary School');
+  }
+  else if (age >= 19) {
     console.log("LHL");
   }
 }
